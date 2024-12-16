873b1b744b4dab9a1df53d7e3b9719897c0d689c
kevinchauhan <rwnkevinchauhan@gmail.com>
2024-12-17 03:51:57 +0530

    add watch content page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home/Home"
 import Login from "./pages/login"
 import Signup from "./pages/Signup"
+import Watch from "./pages/Watch"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
+      <Route path='/watch/:id' element={<Watch />} />
     </Routes>
   )
 }
