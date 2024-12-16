ad326676367c7d3439055c4cb4517f3356afa77a
kevinchauhan <rwnkevinchauhan@gmail.com>
2024-12-17 04:29:21 +0530

    add search page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home/Home"
 import Login from "./pages/login"
 import Signup from "./pages/Signup"
 import Watch from "./pages/Watch"
+import SearchPage from "./pages/Search"
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<Signup />} />
       <Route path='/watch/:id' element={<Watch />} />
+      <Route path='/search' element={<SearchPage />} />
     </Routes>
   )
 }
