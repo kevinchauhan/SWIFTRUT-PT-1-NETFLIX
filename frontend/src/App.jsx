64529bf9e7721aa35b25e98ec43f9d5f26d46d36
kevinchauhan <rwnkevinchauhan@gmail.com>
2024-12-17 04:47:49 +0530

    fix: build issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home/Home"
-import Login from "./pages/login"
+import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import Watch from "./pages/Watch"
 import SearchPage from "./pages/Search"
