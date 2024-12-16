b47f112a3770ec76695b31ef1b7face9bacbc164
kevinchauhan <rwnkevinchauhan@gmail.com>
2024-12-17 00:14:44 +0530

    add search endpoints

--- backend/src/routes/search.js
+++ backend/src/routes/search.js
@@ -1,5 +1,14 @@
-import express from "express";
+import express from 'express';
+import SearchController from '../controllers/SearchController.js';
+import authenticate from '../middlewares/authenticate.js';
+
 const router = express.Router();
 
+// Protect search routes with authenticate middleware
+router.get('/person/:query', authenticate, SearchController.searchPerson);
+router.get('/movie/:query', authenticate, SearchController.searchMovie);
+router.get('/tv/:query', authenticate, SearchController.searchTv);
+router.get('/history', authenticate, SearchController.getSearchHistory);
+router.delete('/history/:id', authenticate, SearchController.removeItemFromSearchHistory);
 
 export default router;
